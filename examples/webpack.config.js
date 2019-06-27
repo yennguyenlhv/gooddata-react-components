f5d0ea356db2803b19572749101d4f5bc5bac193
yennguyenlhv <yen.nguyen@goodata.com>
2019-06-27 13:21:58 +0700

    dual stg3

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -20,7 +20,7 @@ const backendShortcuts = {
     developer: 'https://developer.na.gooddata.com'
 };
 
-const defaultBackend = backendShortcuts.developer;
+const defaultBackend = backendShortcuts.stg3;
 
 function SimplestProgressPlugin() {
     let lastPercent = -10;
