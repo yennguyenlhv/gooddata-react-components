a43c300bc249b4146f01b73f5d2aaa804ef5776e
yennguyenlhv <yen.nguyen@goodata.com>
2019-09-25 11:12:50 +0700

    add change

--- examples/src/components/AttributeFilterExample.jsx
+++ examples/src/components/AttributeFilterExample.jsx
@@ -1,87 +1,63 @@
-// (C) 2007-2019 GoodData Corporation
-import React, { Component } from "react";
-import { LineChart, AttributeFilter, Model, ErrorComponent } from "@gooddata/react-components";
-import { AFM } from "@gooddata/typings";
+import React, { Component } from 'react';
+import { AttributeFilter, ColumnChart } from '@gooddata/react-components';
 
-import "@gooddata/react-components/styles/css/main.css";
-
-import { totalSalesIdentifier, locationResortIdentifier, projectId } from "../utils/fixtures";
-
-const totalSales = Model.measure(totalSalesIdentifier)
-    .format("#,##0")
-    .alias("$ Total Sales");
-
-const locationResort = Model.attribute(locationResortIdentifier);
+import '@gooddata/react-components/styles/css/main.css';
 
 export class AttributeFilterExample extends Component {
     constructor(props) {
         super(props);
-
-        this.onApply = this.onApply.bind(this);
         this.state = {
-            filters: [],
-            error: null,
+            filters: []
         };
     }
 
-    onLoadingChanged(...params) {
-        // eslint-disable-next-line no-console
-        console.info("AttributeFilterExample onLoadingChanged", ...params);
-    }
+    onApply = (filter) => {
+        console.log('AttributeFilterExample filter', filter);
 
-    onApply(filter) {
-        // eslint-disable-next-line no-console
-        console.log("AttributeFilterExample onApply", filter);
-        const isPositiveFilter = AFM.isPositiveAttributeFilter(filter);
-        const inType = isPositiveFilter ? "in" : "notIn";
-        const filterItems = isPositiveFilter
-            ? filter.positiveAttributeFilter[inType]
-            : filter.negativeAttributeFilter[inType];
+        const isPositive = !!filter.in;
+        const elementsProp = isPositive ? 'in' : 'notIn';
 
-        if (!filterItems.length) {
-            if (isPositiveFilter) {
-                this.setState({
-                    error: "The filter must have at least one item selected",
-                });
-            } else {
-                this.setState({ filters: [], error: null });
+        const filters = [{
+            [isPositive ? 'positiveAttributeFilter' : 'negativeAttributeFilter']: {
+                displayForm: {
+                    uri: filter.id
+                },
+                [elementsProp]: filter[elementsProp].map(element => (`<attribute-uri>/elements?id=${element}`))
             }
-        } else {
-            this.setState({ filters: [filter], error: null });
-        }
-    }
+        }];
 
-    onError(...params) {
-        // eslint-disable-next-line no-console
-        console.info("AttributeFilterExample onLoadingChanged", ...params);
+        this.setState({ filters });
     }
 
     render() {
-        const { filters, error } = this.state;
+        const { filters } = this.state;
         return (
-            <div className="s-attribute-filter">
+            <div>
                 <AttributeFilter
+                    identifier="<attribute-displayform-identifier>"
                     projectId={projectId}
-                    filter={Model.negativeAttributeFilter(locationResortIdentifier, [])}
+                    fullscreenOnMobile={false}
                     onApply={this.onApply}
                 />
-                <div style={{ height: 300 }} className="s-line-chart">
-                    {error ? (
-                        <ErrorComponent message={error} />
-                    ) : (
-                        <LineChart
-                            projectId={projectId}
-                            measures={[totalSales]}
-                            trendBy={locationResort}
-                            filters={filters}
-                            onLoadingChanged={this.onLoadingChanged}
-                            onError={this.onError}
-                        />
-                    )}
+                <div style={{ height: 300 }}>
+                    <ColumnChart
+                        projectId="<project-id>"
+                        measures={[{
+                            measure: {
+                                localIdentifier: 'totalSales',
+                                definition: {
+                                    measureDefinition: {
+                                        item: {
+                                            identifier: '<measure-identifier>'
+                                        }
+                                    }
+                                }
+                            }
+                        }]}
+                        filters={filters}
+                    />
                 </div>
             </div>
         );
     }
 }
-
-export default AttributeFilterExample;
